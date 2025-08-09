5521605ec7980ba1a09394c339cc32d476e86279
Rin Iwai <rin@eyrin.jp>
2025-08-09 20:24:16 +0900

    fix

--- include/halx/peripheral/uart/common.hpp
+++ include/halx/peripheral/uart/common.hpp
@@ -11,6 +11,39 @@ enum class UartType {
   DMA,
 };
 
+/**
+ * デフォルトでTx, Rxともに割り込みを使用します。
+ *
+ * @code{.cpp}
+ * #include <cstdio>
+ * #include <halx/core.hpp>
+ * #include <halx/peripheral.hpp>
+ *
+ * extern UART_HandleTypeDef huart2;
+ *
+ * extern "C" void main_thread(void *) {
+ *   using namespace halx::core;
+ *   using namespace halx::peripheral;
+ *
+ *   Uart<&huart2> uart2;
+ *   enable_stdout(uart2);
+ *
+ *   while (true) {
+ *     // 7バイト送信
+ *     uint8_t data[] = {'h', 'e', 'l', 'l', 'o', '\r', '\n'};
+ *     uart2.transmit(data, sizeof(data), MAX_DELAY);
+ *
+ *     // 1バイト受信
+ *     char c;
+ *     if (uart2.receive((uint8_t *)&c, 1, MAX_DELAY)) {
+ *       printf("入力した文字: %c\r\n", c);
+ *     }
+ *
+ *     delay(10);
+ *   }
+ * }
+ * @endcode
+ */
 class UartBase {
 public:
   virtual ~UartBase() {}
