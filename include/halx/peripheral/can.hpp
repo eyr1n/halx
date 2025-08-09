742d7bb04d697bc6a8be5a2517360dc5d168a83d
Rin Iwai <rin@eyrin.jp>
2025-08-09 21:30:02 +0900

    fix

--- include/halx/peripheral/can.hpp
+++ include/halx/peripheral/can.hpp
@@ -19,23 +19,23 @@ template <auto *Handle, class HandleType = decltype(Handle)> class Can;
 template <auto *Handle>
 class Can<Handle, CAN_HandleTypeDef *> : public CanBase {
 public:
-  bool start() override { return bxcan_.start(); }
-  bool stop() override { return bxcan_.stop(); }
+  bool start() override { return can_.start(); }
+  bool stop() override { return can_.stop(); }
   bool transmit(const CanMessage &msg, uint32_t timeout) override {
-    return bxcan_.transmit(msg, timeout);
+    return can_.transmit(msg, timeout);
   }
   std::optional<size_t> attach_rx_filter(const CanFilter &filter,
                                          void (*callback)(const CanMessage &msg,
                                                           void *context),
                                          void *context) override {
-    return bxcan_.attach_rx_filter(filter, callback, context);
+    return can_.attach_rx_filter(filter, callback, context);
   }
   bool detach_rx_filter(size_t filter_index) override {
-    return bxcan_.detach_rx_filter(filter_index);
+    return can_.detach_rx_filter(filter_index);
   }
 
 private:
-  BxCan<Handle> bxcan_;
+  BxCan<Handle> can_;
 };
 
 #endif
@@ -45,23 +45,23 @@ private:
 template <auto *Handle>
 class Can<Handle, FDCAN_HandleTypeDef *> : public CanBase {
 public:
-  bool start() override { return fdcan_.start(); }
-  bool stop() override { return fdcan_.stop(); }
+  bool start() override { return can_.start(); }
+  bool stop() override { return can_.stop(); }
   bool transmit(const CanMessage &msg, uint32_t timeout) override {
-    return fdcan_.transmit(msg, timeout);
+    return can_.transmit(msg, timeout);
   }
   std::optional<size_t> attach_rx_filter(const CanFilter &filter,
                                          void (*callback)(const CanMessage &msg,
                                                           void *context),
                                          void *context) override {
-    return fdcan_.attach_rx_filter(filter, callback, context);
+    return can_.attach_rx_filter(filter, callback, context);
   }
   bool detach_rx_filter(size_t filter_index) override {
-    return fdcan_.detach_rx_filter(filter_index);
+    return can_.detach_rx_filter(filter_index);
   }
 
 private:
-  FdCan<Handle> fdcan_;
+  FdCan<Handle> can_;
 };
 
 #endif
